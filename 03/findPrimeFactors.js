ede991d2e930b617d9c5e10255f62fe21200c898
Ahrling <drowsoul@gmail.com>
2012-11-15 20:29:09 +0100

    fixing some bugs

--- 03/findPrimeFactors.js
+++ 03/findPrimeFactors.js
@@ -1,31 +1,10 @@
 var sys = require('sys');
+var calcFactors = require('./primefactors.js');
 
-var primeNumber = (process.argv[2] === undefined) ? 13195 : process.argv[2];
+var bigNumber = (process.argv[2] === undefined) ? 13195 : process.argv[2];
 
-sys.puts(primeNumber);
+sys.puts(bigNumber);
 
-var factors = [];
-var primes = [1];
-
-var search = 1;
-var factorSum = 1;
-while(factorSum < primeNumber) {
-	
-	isPrime = true;
-	primes.forEach(function(p) {
-		if(p === 1){ return; }
-		if(search % p === 0) {
-			isPrime = false;
-		}
-	});
-	if(isPrime) {
-		primes.push(search);
-		if(primeNumber % search === 0) {
-			factors.push(search);
-			factorSum = factorSum * search;
-		}
-	}
-	search++;
-}
+factors = calcFactors.calcPrimeFactors(bigNumber);
 
 sys.puts(factors);
